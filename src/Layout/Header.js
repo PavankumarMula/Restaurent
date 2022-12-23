34e8e12173f62880496a5bcf297338977957e9ec
PavankumarMula <pavanpappy7@gmail.com>
2022-12-23 15:46:27 +0530

    first commit

---
+++ src/Layout/Header.js
@@ -0,0 +1,14 @@
+import React,{Fragment} from 'react';
+import './Header.css';
+const Header= () => {
+    return <Fragment>
+          <header className='header'>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+          </header>
+          <div>
+            
+          </div>
+        </Fragment>
+}
+export default Header;

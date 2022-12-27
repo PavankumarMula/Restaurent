772f2d20389ac644eee56741d46a87910b2078ae
PavankumarMula <pavanpappy7@gmail.com>
2022-12-27 15:53:09 +0530

    Cart Items

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,18 +1,47 @@
 import React from "react";
 import Modal from "../Ui/Modal";
-import './Cart.css';
-const Cart= props =>{
-    const cartitems=<ul className="item-list">{[{id:"c1",name:'Sushi',amount:2,price:12.99}].map(item=>  <li key={item.id}> {item.name}</li>)}</ul>
-    return <Modal>
-        {cartitems}
-        <div className="total">
-            <span>Total Amount</span>
-            <span>33.99</span>
-        </div>
-        <div className="actions">
-            <button className="button--alt" onClick={props.onHideCart}>Close</button>
-            <button className="but">Order</button>
-        </div>
+import "./Cart.css";
+import { useContext } from "react";
+import CartContext from "../CartItems/Cart-Context";
+
+const Cart = (props) => {
+  const cartctx = useContext(CartContext);
+  const Add = () => {
+    console.log("adding item");
+  };
+  const remove = () => {
+    console.log("removing item");
+  };
+  const totalamount = `$${cartctx.totalamount.toFixed(2)}`;
+  const cartitems = (
+    <ul className="item-list">
+      {cartctx.items.map((item) => (
+        <li key={item.id}>
+          {item.name}
+          <button className="cartbutton" onClick={Add}>
+            +
+          </button>
+          <button className="cartbutton" onClick={remove}>
+            -
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartitems}
+      <div className="total">
+        <span>Total Amount</span>
+        <span>{totalamount}</span>
+      </div>
+      <div className="actions">
+        <button className="button--alt" onClick={props.onHideCart}>
+          Close
+        </button>
+        <button className="but">Order</button>
+      </div>
     </Modal>
-}
+  );
+};
 export default Cart;
--- src/CartItems/CartProvider.js
+++ src/CartItems/CartProvider.js
@@ -9,8 +9,20 @@ const defaultCart={
 const cartReducer = (state,action) =>{
 
  if(action.type ==='ADD'){
-    const updateitems=state.items.concat(action.item);
     const totalamountupdated=state.totalamount+action.item.price*action.item.amount;
+    const existingCartIndex=state.items.findIndex((item)=>item.id===action.item.id);
+    const exisingItem=state.items[existingCartIndex];
+    let updateitems;
+    if(exisingItem){
+      const updateditem= {
+        ...exisingItem,
+        amount:exisingItem.amount+action.item.amount
+      }
+      updateitems=[...state.items]
+      updateitems[existingCartIndex]=updateditem
+    }
+
+     updateitems=state.items.concat(action.item);
     return {items:updateitems,totalamount:totalamountupdated}
  }
 }
